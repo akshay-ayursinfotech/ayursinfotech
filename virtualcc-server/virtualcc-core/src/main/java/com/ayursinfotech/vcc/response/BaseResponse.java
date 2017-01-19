ae0ba8a664c4fa39309d786d1f6e948fba733aa6
Akshay <akshay.perfect101@gmail.com>
2017-01-20 00:13:26 +0530

    parent project added project hierarchy restructured

---
+++ virtualcc-server/virtualcc-core/src/main/java/com/ayursinfotech/vcc/response/BaseResponse.java
@@ -0,0 +1,15 @@
+package com.ayursinfotech.vcc.response;
+
+public class BaseResponse {
+
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
